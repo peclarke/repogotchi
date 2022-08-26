de537fc2586129993369cd2604788815248b6824
Paul Clarke <paulioclarke@icloud.com>
2022-08-26 23:55:30 +1000

    it's only a public repo that everyone will see paul

--- front-end/src/components/RepoListItem.tsx
+++ front-end/src/components/RepoListItem.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 
 export type RepoListItemProps = {
     
 }
 
 export default function RepoListItem(props: RepoListItemProps) {
+    const [elevation, setElevation] = useState(10);
+
     return (
-        <Paper sx = {{ height: 50, padding: 4}} elevation={10}>
-            asd
+        <Paper sx = {{ height: 45, padding: 4, ml: 5}} elevation={elevation}
+               onMouseEnter={() => setElevation(20)}
+               onMouseLeave={() => setElevation(10)}
+        >
+            <Box sx = {{ display: 'flex', flexDirection: 'row' }}>
+                <Avatar sx={{ width: 130, height: 130, ml: -8, mt: -5 }}/>
+                <Box sx = {{ ml: 4 }}>
+                    <Typography>RepositoryOfficialName</Typography>
+                    <Typography>RepoPersonalName, x/y Commits</Typography>
+                </Box>
+            </Box>
         </Paper>
     )
 }
--- front-end/src/screens/RepoListScreen.tsx
+++ front-end/src/screens/RepoListScreen.tsx
@@ -4,20 +4,25 @@ import React from 'react';
 import BigButton from '../components/actions/BigButton';
 import ManageRepos from '../components/actions/ManageRepos';
 import RepoListItem from '../components/RepoListItem';
+import useWindowDimensions from '../hooks/useWindowDimensions';
 
 export type RepoListScreenProps = {
     
 }
 
 export default function RepoListScreen(props: RepoListScreenProps) {
-
+    const { height } = useWindowDimensions();
 
     return (
         <>
-        <Box sx={{ flexGrow: 1, mt: 5 }}>
+        <Box sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
                 <Grid item xs={8}>
-                    <Box sx = {{ ml: 5, justifyContent: 'space-between', display: 'flex', flexDirection: 'column', height: 500, backgroundColor: 'red'}}>
+                    <Box sx = {{ justifyContent: 'space-evenly', display: 'flex',
+                                 flexDirection: 'column', height: height, background: "linear-gradient(#e66465, #9198e5)",
+                                 paddingLeft: 4, paddingRight: 4}}>
+                        {/* Render this with a proper list element from mui  */}
+                        <RepoListItem />
                         <RepoListItem />
                         <RepoListItem />
                     </Box>
