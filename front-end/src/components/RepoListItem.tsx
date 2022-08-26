d8af8c84df6dafcaa30001aacf3d0b699eeb2f32
Paul Clarke <paulioclarke@icloud.com>
2022-08-26 23:26:42 +1000

    hooks and shiz

---
+++ front-end/src/components/RepoListItem.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Paper from '@mui/material/Paper';
+
+export type RepoListItemProps = {
+    
+}
+
+export default function RepoListItem(props: RepoListItemProps) {
+    return (
+        <Paper sx = {{ height: 50, padding: 4}} elevation={10}>
+            asd
+        </Paper>
+    )
+}
--- front-end/src/screens/RepoListScreen.tsx
+++ front-end/src/screens/RepoListScreen.tsx
@@ -3,6 +3,7 @@ import Grid from '@mui/material/Grid';
 import React from 'react';
 import BigButton from '../components/actions/BigButton';
 import ManageRepos from '../components/actions/ManageRepos';
+import RepoListItem from '../components/RepoListItem';
 
 export type RepoListScreenProps = {
     
@@ -16,8 +17,9 @@ export default function RepoListScreen(props: RepoListScreenProps) {
         <Box sx={{ flexGrow: 1, mt: 5 }}>
             <Grid container spacing={2}>
                 <Grid item xs={8}>
-                    <Box sx = {{ ml: 5 }}>
-
+                    <Box sx = {{ ml: 5, justifyContent: 'space-between', display: 'flex', flexDirection: 'column', height: 500, backgroundColor: 'red'}}>
+                        <RepoListItem />
+                        <RepoListItem />
                     </Box>
                 </Grid>
                 <Grid item xs={4}>
