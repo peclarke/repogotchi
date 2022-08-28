990c48a65147bdc97df8364dfe377490e4ed9000
ellimiri <ellimiri@outlook.com>
2022-08-28 11:40:16 +1000

    Turns repogotchi display into its own widget

--- front-end/src/components/Repogotchi.tsx
+++ front-end/src/components/Repogotchi.tsx
@@ -35,6 +35,36 @@ export function ColourLayer(props: ColourLayerProps) {
     );
 }
 
+export type RepogotchiDisplayProps = {
+    repo: RepogotchiType;
+    imgWidth: number;
+    containerHeight: number;
+}
+
+export function RepogotchiDisplay(props: RepogotchiDisplayProps) {
+    const mouthWidth = props.imgWidth / 4;
+    const mouthLeft = props.imgWidth / 2.65;
+    const mouthTop = props.imgWidth / 2.65;
+
+    const earsSrc = require('../sprite/ear' + props.repo.Ears + '.png');
+    const earsMaskSrc = require('../sprite/earmask' + props.repo.Ears + '.png');
+    const bodySrc = require('../sprite/body' + props.repo.Body + '.png');
+    const bodyMaskSrc = require('../sprite/bodymask' + props.repo.Body + '.png');
+    const mouthSrc = require('../sprite/mouths/mouth' + props.repo.Mouth + '.png');
+    const eyesSrc = require('../sprite/eye' + props.repo.Eyes + '.png');
+    const accessorySrc = require('../sprite/accessory' + props.repo.Accessory + '.png');
+
+    return (<Box display="inline" alignItems="center" justifyContent="center" position="relative" style={{ width: "100%", height: props.containerHeight }}>
+        <ColourLayer height={1} src={earsMaskSrc} width={props.imgWidth} colour={props.repo.Colour} />
+        <SpriteLayer height={2} src={earsSrc} width={props.imgWidth} />
+        <ColourLayer height={3} src={bodyMaskSrc} width={props.imgWidth} colour={props.repo.Colour} />
+        <SpriteLayer height={4} src={bodySrc} width={props.imgWidth} />
+        <img alt="mouth" src={mouthSrc} style={{ position: 'absolute', zIndex: 5, objectFit: 'scale-down', width: mouthWidth, transform: "rotateX(180deg)", left: mouthLeft, top: mouthTop }} />
+        <SpriteLayer height={6} src={eyesSrc} width={props.imgWidth} />
+        <SpriteLayer height={7} src={accessorySrc} width={props.imgWidth} />
+    </Box>);
+}
+
 export type RepogotchiProps = {
     name: string;
     repo: RepogotchiType;
@@ -45,17 +75,6 @@ export default function Repogotchi(props: RepogotchiProps) {
     const { width, height } = useWindowDimensions();
 
     const imgWidth = width / 3 - 50;
-    const mouthWidth = imgWidth / 4;
-    const mouthLeft = imgWidth / 2.65;
-    const mouthTop = imgWidth / 2.65;
-
-    const earsSrc = require('../sprite/ear' + props.repo.Ears + '.png');
-    const earsMaskSrc = require('../sprite/earmask' + props.repo.Ears + '.png');
-    const bodySrc = require('../sprite/body' + props.repo.Body + '.png');
-    const bodyMaskSrc = require('../sprite/bodymask' + props.repo.Body + '.png');
-    const mouthSrc = require('../sprite/mouths/mouth' + props.repo.Mouth + '.png');
-    const eyesSrc = require('../sprite/eye' + props.repo.Eyes + '.png');
-    const accessorySrc = require('../sprite/accessory' + props.repo.Accessory + '.png');
 
     const commitProgress = Math.floor(props.repo.LevelProgress / props.repo.LevelReq) * 100;
 
@@ -64,15 +83,7 @@ export default function Repogotchi(props: RepogotchiProps) {
             <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" style={{ marginTop: 50 }}>
                 <Typography variant="h5">{props.name}</Typography>
                 <br></br>
-                <Box display="inline" alignItems="center" justifyContent="center" position="relative" style={{ width: "100%", height: 500 }}>
-                    <ColourLayer height={1} src={earsMaskSrc} width={imgWidth} colour={props.repo.Colour} />
-                    <SpriteLayer height={2} src={earsSrc} width={imgWidth} />
-                    <ColourLayer height={3} src={bodyMaskSrc} width={imgWidth} colour={props.repo.Colour} />
-                    <SpriteLayer height={4} src={bodySrc} width={imgWidth} />
-                    <img alt="mouth" src={mouthSrc} style={{ position: 'absolute', zIndex: 5, objectFit: 'scale-down', width: mouthWidth, transform: "rotateX(180deg)", left: mouthLeft, top: mouthTop }} />
-                    <SpriteLayer height={6} src={eyesSrc} width={imgWidth} />
-                    <SpriteLayer height={7} src={accessorySrc} width={imgWidth} />
-                </Box>
+                <RepogotchiDisplay repo={props.repo} imgWidth={imgWidth} containerHeight={500} />
                 <CommitProgress progress={commitProgress} repo={props.repo} />
             </Box>
         </ThemeProvider>
