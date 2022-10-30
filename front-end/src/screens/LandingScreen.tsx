b7f360210e00c277de1743e7f12343fd1507b2be
Paul Clarke <paulioclarke@icloud.com>
2022-10-30 22:24:48 +1000

    comment cleanup

--- front-end/src/screens/LandingScreen.tsx
+++ front-end/src/screens/LandingScreen.tsx
@@ -55,6 +55,31 @@ theme.typography.h2 = {
 
 // }
 
+const exampleRepo: RepogotchiType = {
+    GithubName: "MyFunRepository",
+    PersonalName: "Mittens",
+    Age: 9,
+    Languages: [""],
+    MaxHealth: 100,
+    CurrentHealth: 70,
+    CommitProgress: 100,
+    LastCommit: "",
+    Level: 0,
+    Birthdate: "",
+    LevelProgress: 0,
+    LevelReq: 0,
+    LastVisit: "",
+    Affection: 15,
+    MaxAffection: 20,
+    Owner: "",
+    Body: 1,
+    Eyes: 0,
+    Mouth: 0,
+    Accessory: 0,
+    Ears: 2,
+    Colour: "pink",
+}
+
 export const styles = {
     button: {
         color: theme.palette.getContrastText("#9e5482"),
@@ -76,19 +101,11 @@ export type LandingScreenProps = {
 export default function LandingScreen(props: LandingScreenProps) {
     const { width, height } = useWindowDimensions();
 
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState("");
-
     const [repoIndex, setRepoIndex] = useState(0);
 
     const app = initializeApp(firebaseConfig);
     const db: Firestore = getFirestore(app);
-
     const auth = getAuth();
-
-    // useEffect(() => {
-
-    // }, [auth.currentUser])
 
     useEffect(() => {
         if (localStorage.getItem("email")) {
@@ -106,145 +123,6 @@ export default function LandingScreen(props: LandingScreenProps) {
     }, [])
 
     const nav = useNavigate();
-
-    // const login =  () => {
-    //     setLoading(true);
-
-    //     const work = async () => {
-
-    //         const docRef = doc(db, "users/" + email);
-    //         const snapDoc = await getDoc(docRef);
-
-
-    //         const auth = getAuth()
-    //         setPersistence(auth, browserLocalPersistence).then(() => {
-    //             if (snapDoc.exists()) {
-    //                 // auth the user
-                    // signInWithEmailAndPassword(auth, email, pass)
-                    //     .then((userCredential) => {
-                    //         const user = userCredential.user;
-                    //     })
-                    //     .catch((error) => {
-                    //         const errorCode = error.code;
-                    //         const errorMessage = error.message;
-                    //         setError(errorMessage);
-                    //     });
-                    
-    //                 // onAuthStateChanged(auth, (user) => {
-    //                 //     const data = snapDoc.data();
-
-    //                 //     if ((data.github === username) && user) {
-    //                 //         localStorage.setItem('user', username);
-    //                 //         localStorage.setItem('email', email);
-    //                 //         // setLoading(false);
-    //                 //         nav('/home')
-    //                 //     } else {
-    //                 //         nav("/")
-    //                 //         setLoading(false);
-    //                 //         signOut(auth).then(() => {
-    //                 //             localStorage.clear();
-    //                 //             // setLoading(false);
-    //                 //             // setError("A user already exists with this email. But, wrong github username")
-    //                 //             // nav("/")
-    //                 //             // Sign-out successful.
-    //                 //           }).catch((error) => {
-    //                 //             // An error happened.
-    //                 //           });
-    //                 //     }
-
-    //                     // if (data.github !== username) {
-    //                     //     localStorage.clear()
-    //                     //     setError("");
-    //                     //     setLoading(false);
-    //                     // }
-    //                     // setLoading(false);
-    //             } else {
-    //                 // create the user
-    //                 createUserWithEmailAndPassword(auth, email, pass)
-    //                     .then((userCredential) => {
-    //                         const user = userCredential.user;
-    //                         const userDoc = doc(db, "users/" + email)
-
-    //                     })
-    //                     .catch((error) => {
-    //                         const errorCode = error.code;
-    //                         const errorMessage = error.message;
-    //                         setError(errorMessage);
-    //                     });
-
-    //                 const userDoc = doc(db, "users/" + email)
-    //                 const set_the_things = async () => {
-    //                     await setDoc(userDoc, {'username': email, 'github': username});
-    //                 }
-
-    //                 // onAuthStateChanged(auth, (user) => {
-    //                 //     if (user) {
-    //                 //         set_the_things();
-    //                 //         localStorage.setItem('user', username);
-    //                 //         localStorage.setItem('email', email);
-    //                 //         nav('/home')
-    //                 //     } else {
-    //                 //     //   setLoading(false);
-    //                 //     }
-    //                 //     // setLoading(false);
-    //                 //   });
-    //             }
-    //         })
-
-    //         onAuthStateChanged(auth, (user) => {
-    //             const data = snapDoc.data();
-
-    //             if (data && user) {
-    //                 if (data.github === username) {
-    //                     localStorage.setItem('user', username);
-    //                     localStorage.setItem('email', email);
-    //                     setLoading(false);
-    //                     nav('/home')
-    //                 } else {
-    //                     nav("/")
-    //                     setLoading(false);
-    //                     console.log("ASDHASJDAS");
-    //                     signOut(auth).then(() => {
-    //                         // localStorage.clear();
-    //                         // setLoading(false);
-    //                         // setError("A user already exists with this email. But, wrong github username")
-    //                         // nav("/")
-    //                         // Sign-out successful.
-    //                     }).catch((error) => {
-    //                         // An error happened.
-    //                     });
-    //                 }
-    //             }
-    //         })
-    //     }
-
-    //     work();
-    // }
-
-    const exampleRepo: RepogotchiType = {
-        GithubName: "MyFunRepository",
-        PersonalName: "Mittens",
-        Age: 9,
-        Languages: [""],
-        MaxHealth: 100,
-        CurrentHealth: 70,
-        CommitProgress: 100,
-        LastCommit: "",
-        Level: 0,
-        Birthdate: "",
-        LevelProgress: 0,
-        LevelReq: 0,
-        LastVisit: "",
-        Affection: 15,
-        MaxAffection: 20,
-        Owner: "",
-        Body: 1,
-        Eyes: 0,
-        Mouth: 0,
-        Accessory: 0,
-        Ears: 2,
-        Colour: "pink",
-    }
 
     return (
         <form>
@@ -282,7 +160,3 @@ export default function LandingScreen(props: LandingScreenProps) {
         </form>
     )
 }
-
-
-// jazz: createColor("#e66465"),
-//         funk: createColor("#9198e5")
